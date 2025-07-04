aaf588c39b4c50443019aedf2b7026a0f49bbc30
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-07-04 16:00:11 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,10 @@ app.use('/api/users', require('./routes/userRoute'));
 app.use('/api', require('./routes/eventRoute'));
 
 if (process.env.RUN_SEED === 'true') {
-  require('./seed')();
+  require('./seed');
+  require('./adminSeed');
 }
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
