34e4a1a00e5d61c8580c493dcc74425537033283
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-07-04 19:39:49 +0800

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,7 +5,7 @@ const { auth } = require('../middleware/authMiddleware');
 
 router.post('/register', userCtrl.register);
 router.post('/login', userCtrl.login);
-router.get('/events', auth,userCtrl.getEvents);
+router.get('/events', userCtrl.getEvents);
 router.post('/register-event', auth, userCtrl.registerEvent);
 router.get('/my-registrations', auth, userCtrl.getMyRegistrations);
 
