666342fe333d01308e701e3bd394aa7d4087d1c5
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-07-04 15:56:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('/api/users', require('./routes/userRoute'));
 app.use('/api', require('./routes/eventRoute'));
 
 if (process.env.RUN_SEED === 'true') {
-  require('./seeder')();
+  require('./seed')();
 }
 
 const PORT = process.env.PORT || 5000;
